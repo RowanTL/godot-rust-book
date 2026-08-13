49bd0208f1e5d991e8867146a44cbd2775309325
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-08-13 09:50:16 -0700

    Switch examples f64 -> f32

--- src/intro/code/hello-world-phpr-update.rs
+++ src/intro/code/hello-world-phpr-update.rs
@@ -1,5 +1,3 @@
-// Note: This code contains the updated version of the physics_process function.
-
 // ANCHOR: entry-point
 // ANCHOR: class-declaration
 use godot::prelude::*;
@@ -17,10 +15,10 @@ use godot::classes::Sprite2D;
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
 struct Player {
-    speed: f64,
-    angular_speed: f64,
+    speed: f32,
+    angular_speed: f32,
 
-    base: Base<Sprite2D>
+    base: Base<Sprite2D>,
 }
 // ANCHOR_END: class-declaration
 
@@ -31,21 +29,23 @@ use godot::classes::ISprite2D;
 
 #[godot_api]
 impl ISprite2D for Player {
-// ANCHOR_END: init
-// ANCHOR_END: physics-process
-// ANCHOR_END: rotate
+    // ANCHOR_END: init
+    // ANCHOR_END: physics-process
+    // ANCHOR_END: rotate
     // ANCHOR: init
     fn init(base: Base<Sprite2D>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
 
         Self {
             speed: 400.0,
-            angular_speed: std::f64::consts::PI,
+            angular_speed: std::f32::consts::PI,
             base,
         }
     }
     // ANCHOR_END: init
 
+    // This snippet contains the updated physics_process block.
+    // delta may also be f64.
     // ANCHOR: physics-process
     fn physics_process(&mut self, delta: f32) {
         // GDScript code:
@@ -54,7 +54,7 @@ impl ISprite2D for Player {
         // var velocity = Vector2.UP.rotated(rotation) * speed
         // position += velocity * delta
 
-        let radians = self.angular_speed as f32 * delta;
+        let radians = self.angular_speed * delta;
         self.base_mut().rotate(radians);
 
         let rotation = self.base().get_rotation();
@@ -68,16 +68,17 @@ impl ISprite2D for Player {
         // );
     }
     // ANCHOR_END: physics-process
-// ANCHOR: init
+    // ANCHOR: init
 }
 // ANCHOR_END: init
 // ANCHOR_END: rotate
 
+// amount is allowed to be f64 too
 // ANCHOR: custom-api
 #[godot_api]
 impl Player {
     #[func]
-    fn increase_speed(&mut self, amount: f64) {
+    fn increase_speed(&mut self, amount: f32) {
         self.speed += amount;
         self.signals().speed_increased().emit();
     }
--- src/intro/code/hello-world.rs
+++ src/intro/code/hello-world.rs
@@ -15,10 +15,10 @@ use godot::classes::Sprite2D;
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
 struct Player {
-    speed: f64,
-    angular_speed: f64,
+    speed: f32,
+    angular_speed: f32,
 
-    base: Base<Sprite2D>
+    base: Base<Sprite2D>,
 }
 // ANCHOR_END: class-declaration
 
@@ -29,42 +29,44 @@ use godot::classes::ISprite2D;
 
 #[godot_api]
 impl ISprite2D for Player {
-// ANCHOR_END: init
-// ANCHOR_END: physics-process
-// ANCHOR_END: rotate
+    // ANCHOR_END: init
+    // ANCHOR_END: physics-process
+    // ANCHOR_END: rotate
     // ANCHOR: init
     fn init(base: Base<Sprite2D>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
 
         Self {
             speed: 400.0,
-            angular_speed: std::f64::consts::PI,
+            angular_speed: std::f32::consts::PI,
             base,
         }
     }
     // ANCHOR_END: init
 
+    // delta may also be f64.
     // ANCHOR: physics-process
     fn physics_process(&mut self, delta: f32) {
         // In GDScript, this would be:
         // rotation += angular_speed * delta
 
-        let radians = self.angular_speed as f32 * delta;
+        let radians = self.angular_speed * delta;
         self.base_mut().rotate(radians);
         // The 'rotate' method requires a f32,
         // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
     }
     // ANCHOR_END: physics-process
-// ANCHOR: init
+    // ANCHOR: init
 }
 // ANCHOR_END: init
 // ANCHOR_END: rotate
 
+// amount is allowed to be f64 too
 // ANCHOR: custom-api
 #[godot_api]
 impl Player {
     #[func]
-    fn increase_speed(&mut self, amount: f64) {
+    fn increase_speed(&mut self, amount: f32) {
         self.speed += amount;
         self.signals().speed_increased().emit();
     }
